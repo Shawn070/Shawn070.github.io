d1d208a60f48216beeb5a40f213fd011df912cf0
Shawn-PC <307882919@qq.com>
2019-08-14 10:21:27 +0800

    Site updated: 2019-08-14 10:21:22

--- dist/music.js
+++ dist/music.js
@@ -1,6 +1,6 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    min: true,
+    min: false,
     autoplay: false,
     loop: 'all',
     volume: 0.7,
@@ -8,7 +8,7 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
       {
-        name: '222',
+        name: '333',
         artist: '麦小兜',
         url: 'http://up.mcyt.net/?down/45967.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
